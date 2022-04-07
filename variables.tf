601c6e98dcb3517e1613e5c2cc5d92698c8e3ae9
Sudhir Jangam <sudhirj8@gmail.com>
2022-04-07 10:01:25 -0400

    adding vpc

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "aws_access_key" {}
 
 variable "aws_secret_key" {}
+
 variable "region" {
     default = "us-east-1"
 }
@@ -12,3 +13,6 @@ variable "vpc_cidr" {
 variable "subnet1_cidr" {
   default = "172.16.0.0/24"
 }
+
+# variables
+# 
