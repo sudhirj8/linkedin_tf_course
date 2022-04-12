831c1e7347aa9682da22f022c09a1db12e358c6f
Sudhir Jangam <sudhirj8@gmail.com>
2022-04-12 14:02:31 -0400

    corrected asg section

--- vpc.tf
+++ vpc.tf
@@ -114,7 +114,9 @@ resource "aws_launch_template" "web-server" {
 }
 
 resource "aws_autoscaling_group" "asg-web" {
-  launch_template = aws_launch_template.web-server.id
+  launch_template = { 
+    id = aws_launch_template.web-server.id
+  }
   availability_zones   = data.aws_availability_zones.available.names
   min_size = 2
   max_size = 5
